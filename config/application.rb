b3b39327fa0ce78044c79ed6c645a7c5dafb399f
Leonel Sanches <113376080+seg-leonelsanches@users.noreply.github.com>
2022-10-26 15:06:08 -0700

    Using .env to set the Write Key.

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,13 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# Load dotenv only in development or test environment
+if ['development', 'test'].include? ENV['RAILS_ENV']
+  Dotenv::Railtie.load
+end
+
+SEGMENT_WRITE_KEY = ENV['SEGMENT_WRITE_KEY']
+
 module LeosMusicShopRuby
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
--- config/initializers/analytics_ruby.rb
+++ config/initializers/analytics_ruby.rb
@@ -1,4 +1,4 @@
 Analytics = Segment::Analytics.new({
-    write_key: 'Fc93iGPeXXJrLGy1wzTAZJw9IAybykP8',
+    write_key: ENV['SEGMENT_WRITE_KEY'],
     on_error: Proc.new { |status, msg| print msg }
 })
